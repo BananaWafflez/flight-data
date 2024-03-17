fc4ba6ad759f3d070f571bb7e3184d49768a98ec
BananaWafflez <113249309+BananaWafflez@users.noreply.github.com>
2024-03-17 10:49:30 -0400

    Delete api script.py

--- api script.py
+++
@@ -1,5 +0,0 @@
-import requests
-auth = "31b5587f2d0365a4c74faf808f14582b1c8a6248e61a945b99ce14b759fbdac9fce822ce20fb5cdeebbcde817d85f80c"
-
-response = requests.get("https://airportdb.io/api/v1/airport/KJFK?apiToken=" + auth)
-print(response.json())
