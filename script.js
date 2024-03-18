27ca097986a3e3dc553e29964fa225cc377b8c1c
Bentley <benaing12@gmail.com>
2024-03-18 19:13:48 -0400

    saved api call into variable

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const apiToken = "31b5587f2d0365a4c74faf808f14582b1c8a6248e61a945b99ce14b759fbdac9fce822ce20fb5cdeebbcde817d85f80c";
-fetch('https://airportdb.io/api/v1/airport/KTPA?apiToken=' + apiToken)
+var jsonstring;
+async function foo(){
+    await fetch('https://airportdb.io/api/v1/airport/KTPA?apiToken=' + apiToken)
     .then(response=>{
         if(response.ok){
+            
             return response.json();
         }else{
             throw new Error('API Request failed');
         }
     })
     .then(data=>{
-        console.log(data);
+        jsonstring = data;
+        setData(data);
+    })
+    .then(()=>{
+        console.log(jsonstring);
     })
     .catch(error=>{
-        console.log(data);
+        console.log(error);
     })
+    
+
+} 
+function setData(data){
+    jsonstring = data;
+}
+foo();
+
+    
